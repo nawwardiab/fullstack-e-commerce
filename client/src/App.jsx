e9f32cc6d58e276891e617d322607c56e9a328ce
Manuela Di Dario <manueladidario@Manuelas-MacBook-Air.local>
2025-02-11 23:22:10 +0100

    conflicts resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { FavoriteProvider } from "./context/FavoriteContext";
+import { AuthProvider } from "./context/AuthContext";
+
 import Navbar from "./components/Navabar";
-import Searchpage from "./pages/Searchpage"; 
-import Homepage from "./pages/Homepage"; 
-import { FavoriteProvider } from "./contexts/FavoriteContext";
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Homepage from "./pages/Homepage"
-import { AuthProvider, AuthContext } from './context/AuthContext';
-
-
-const ProtectedRoute = ({ children }) => {
-  const { token } = useContext(AuthContext);
-  return token ? children : <Navigate to="/login" />;
-};
-
+import Homepage from "./pages/Homepage";
+import Searchpage from "./pages/Searchpage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
     <AuthProvider>
       <FavoriteProvider>
-        <Router>
           <Navbar />
           <Routes>
             <Route path="/" element={<Homepage />} />
@@ -32,7 +22,6 @@ const App = () => {
             {/* Redirect any unknown routes to /login */}
             <Route path="*" element={<Navigate to="/login" />} />
           </Routes>
-        </Router>
       </FavoriteProvider>
     </AuthProvider>
   );
